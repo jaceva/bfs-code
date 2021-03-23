3524b045c18134a06764ef0d8a7bdd0b09fbcd90
Jace van Auken <jaceva@Jaces-Mac-mini.fios-router.home>
2021-03-23 16:24:27 -0400

    Added initial_path variable to frontier initialization section

--- bfs_final.py
+++ bfs_final.py
@@ -8,7 +8,8 @@ def bfs(root_node, goal_value):
   path_queue = deque()
 
   # add root path to the queue
-  path_queue.appendleft([root_node])
+  initial_path = [root_node]
+  path_queue.appendleft(initial_path)
   
   # create a while loop that continues
   # as long as there are paths in the queue
